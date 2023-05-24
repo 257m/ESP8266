6feef5914aa4b0d1ba6d75f5402a5c9c5d79eefb
257m <107591653+257m@users.noreply.github.com>
2023-05-24 17:49:23 -0400

    Setup the loop and user_init

--- src/main.c
+++ src/main.c
@@ -9,6 +9,16 @@
 /// custom function for the web server
 #include "serial_io.h"
 
+// Is used for the ets_loop_task which is explained inside of user_init
+#define LOOP_TASK_PRIORITY 1
+#define LOOP_QUEUE_LENGTH 1
+static ETSEvent loop_queue[LOOP_QUEUE_LENGTH];
+
+static void loop_task(ETSEvent* events)
+{
+	
+}
+
 /* 
 *	The SDK calls user_init as the entry point function for the user
 *	The SDK runs a watchdog timer on the function and we don't return in
@@ -16,6 +26,7 @@
 */
 void user_init()
 {
+	// Initalize uart and set baud rate by setting the uart clock divider
 	uart_init(115200);
 
 	/*
@@ -26,7 +37,33 @@ void user_init()
 	*	of ours the SDK will call. user_init is only to setup the actual
 	*	program. The SDK has a function that we can pass a function pointer
 	*	to and it will call that function in a loop. Very similar to the arduino
-	*	loop. Here is the function:
+	*	loop. 
+	*	Here is the function:
 	*	bool ets_task(ETSTask task, uint8 prio, ETSEvent *queue, uint8 qlen);
+	*	
+	*	Here are the definitions for some the data structures:
+	*	// ETSSignal and ETSParam are both unsigned 32 bit integers
+	*	typedef struct {
+	*		ETSSignal sig;
+	*		ETSParam  par;
+	*	} ETSEvent;
+	*
+	*	// ETSTask is just function pointer that pointer to a function
+	*	// that takes in a pointer to a ETSEvent (essentially a method with no args)
+	*	typedef void (*ETSTask)(ETSEvent *e);
+	*
+	*	In the ets_task function:
+	*	The ETSTask task argument is the function that is going to be
+	*	called in a loop by the SDK.
+	*	
+	*	The uint8 prio is the priority that the function will have
+	*	over other loops
+	*	
+	*	ETSEvent* queue is the queue pointer that is going to be
+	*	passed to ETSTask task function in a loop
+	*
+	*	uint8 qlen is the size/length of the queue
 	*/
+
+	ets_loop_task(loop_task, LOOP_TASK_PRIORITY, loop_queue, LOOP_QUEUE_LENGTH);
 }
