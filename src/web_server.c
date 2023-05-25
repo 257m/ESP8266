4e1260fc30a002b134a751d9baa68240ecb6cede
257m <107591653+257m@users.noreply.github.com>
2023-05-25 11:03:54 -0400

    Added messages in receve for debugging

--- src/web_server.c
+++ src/web_server.c
@@ -35,6 +35,7 @@ void web_server_receive(void *arg, char *pusrdata, unsigned short length)
 {
 	struct espconn* esp_conn = arg;
 	espconn_set_opt(esp_conn, ESPCONN_REUSEADDR);
+	printf("Received data:\r\n");
 	const char html [] = "<!DOCTYPE html><html>hello, world</html>";
 	const char* header = aprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\nConnection: close\r\nContent-Length: %ld\r\n\r\n", sizeof(html));
 	unsigned int buf_size = sizeof(html) + str_len(header) + 1;
@@ -45,6 +46,7 @@ void web_server_receive(void *arg, char *pusrdata, unsigned short length)
 	while (i < buf_size)
 		buf[i] = header[i - sizeof(html)];
 	free(header);
+	printf("Sending now\r\n");
 	espconn_sent(esp_conn, buf, buf_size);
 }
 
