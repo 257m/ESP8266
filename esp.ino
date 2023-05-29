517726141b652e524cb5106f4dd39e941c95eedd
257m <107591653+257m@users.noreply.github.com>
2023-05-29 19:42:19 -0400

    Update and rename src/main.ino to esp.ino

---
+++ esp.ino
@@ -0,0 +1,26 @@
+extern "C" {
+	/// Proprietary SDK includes
+	#include <ets_sys.h>
+	#include <espconn.h>
+	#include <os_type.h>
+	#include <osapi.h>
+	#include <mem.h>
+	#include <user_interface.h>
+
+	/// Custom libraries for uart_init, printf, gets, etc...	
+	#include "uart_io.h"
+	#include "serial_io.h"
+	#include "web_server.h"
+}
+
+void setup()
+{
+	uart_init(9600);
+	serial_write("Uart is functional\r\n");
+	web_server_init();
+}
+
+void loop()
+{
+
+}
