b9daedf8c3fb2cd6d8b107b8148a08048543ac68
257m <1alimaze@gmail.com>
2023-06-06 12:19:47 -0400

    Fingers crossed it dosen't break due to arduino ide being dumb

---
+++ lib/pin_io.h
@@ -0,0 +1,9 @@
+#ifndef PIN_IO_H
+#define PIN_IO_H
+
+#define PERIPHS_GPIO_BASEADDR 0x60000300
+
+#define WRITE_PERI_REG(addr, val) (*((volatile unsigned int*)addr)) = (unsigned int)(val)
+#define GPIO_REG_WRITE(reg, val) WRITE_PERI_REG(PERIPHS_GPIO_BASEADDR + reg, val)
+
+#endif /* PIN_IO_H */
