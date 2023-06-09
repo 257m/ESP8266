3cb64896caa926793350df80ae93669caa2b7333
Maaz Ali <alim2790@wrdsb.ca>
2023-06-09 14:20:33 -0400

    jsjsjsj

--- esp_web_server/esp_web_server.ino
+++ esp_web_server/esp_web_server.ino
@@ -6,6 +6,7 @@ extern "C" {
 	#include "osapi.h"
 	#include "mem.h"
 	#include "user_interface.h"
+  #include "espnow.h"
 
 	/// Custom libraries for uart_init, printf, gets, etc...	
 	#include "uart_io.h"
@@ -17,17 +18,18 @@ typedef struct {
 	int x, y;
 } Message;
 
-void on_data_recv(const uint8_t* mac, Message* incomingData, int len)
+void on_data_recv(uint8_t* mac, uint8_t* data, unsigned char len)
 {
-	printf("%d, %d\n", x, y);
+	printf("%d, %d\n", ((Message*)data)->x, ((Message*)data)->y);
 }
 
 void setup()
 {
-	uart_init(9600);
+	//uart_init(9600);
+  Serial.begin(9600);
 	//serial_write("Uart is functional\r\n");
 	web_server_init("EspWebServer", "PASSWORD", 6, false);
-	if (esp_now_init()) {
+	if (esp_now_init())
 		printf("Error initializing ESP-NOW");
 
 	esp_now_register_recv_cb(on_data_recv);	
