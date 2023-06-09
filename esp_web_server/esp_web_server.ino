d4977df7b5ea3cdcdc82cfa6e09e66eb8ab0200a
257m <1alimaze@gmail.com>
2023-06-09 14:02:04 -0400

    Added esp now to server

--- esp_web_server/esp_web_server.ino
+++ esp_web_server/esp_web_server.ino
@@ -13,13 +13,27 @@ extern "C" {
 	#include "web_server.h"
 }
 
+typedef struct {
+	int x, y;
+} Message;
+
+void on_data_recv(const uint8_t* mac, Message* incomingData, int len)
+{
+	printf("%d, %d\n", x, y);
+}
+
 void setup()
 {
 	uart_init(9600);
 	//serial_write("Uart is functional\r\n");
 	web_server_init("EspWebServer", "PASSWORD", 6, false);
+	if (esp_now_init()) {
+		printf("Error initializing ESP-NOW");
+
+	esp_now_register_recv_cb(on_data_recv);	
 }
 
 void loop()
 {
+
 }
