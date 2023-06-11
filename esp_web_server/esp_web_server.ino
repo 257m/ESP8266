63ba12043758af39f64c3ced2243c69898d98481
257m <1alimaze@gmail.com>
2023-06-11 14:31:13 -0400

    Converted byte array to Message structure

--- esp_web_server/esp_web_server.ino
+++ esp_web_server/esp_web_server.ino
@@ -14,6 +14,12 @@ extern "C" {
 	#include "web_server.h"
 }
 
+void mem_cpy(unsigned char* dest, unsigned char* source, unsigned int len)
+{
+	while (len--)
+		*dest++ = *source++;
+}
+
 unsigned char server_mac [] = {0x2e,0xf4,0x32,0x4c,0xbe,0x11};
 
 typedef struct {
@@ -22,19 +28,17 @@ typedef struct {
 
 void on_data_recv(uint8_t* mac, uint8_t* data, unsigned char len)
 {
-	Message* m = (Message*)data;
+	Message m;
+	// Casts because -fpermissive
+	mem_cpy((unsigned char*)&m, (unsigned char*)data, len);
 	printf("MAC_ADDR: %x:%x:%x:%x\r\n", mac[0], mac[1], mac[2], mac[3]);
 	printf("Length: %d\r\n", len);
-	printf("DATA:\r\n");
-	for (unsigned int i = 0;i < len;i++)
-		printf("%d:\t%d\r\n", i, data[i]);
+	printf("DATA: %d, %d\r\n", m.x, m.y);
 }
 
 void setup()
 {
 	uart_init(9600);
-	//serial_write("Uart is functional\r\n");
-	//wifi_set_macaddr(AP_IF, server_mac);
 	web_server_init("EspWebServer", "PASSWORD", 6, false);
 	if (esp_now_init())
 		printf("Error initializing ESP-NOW");
