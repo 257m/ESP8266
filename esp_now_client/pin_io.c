4929756bcd6332ef20383ac5b938d341495c6f48
257m <1alimaze@gmail.com>
2023-06-13 13:10:04 -0400

    Fixed pin io

--- esp_now_client/pin_io.c
+++ esp_now_client/pin_io.c
@@ -2,7 +2,7 @@
 
 inline void set_pin(unsigned char pin, bool state)
 {
-	GPIO_REG_WRITE(GPIO_OUT_W1TS + (0x4*state), (1 << pin)); 		
+	GPIO_REG_WRITE(GPIO_OUT_W1TS + (0x4*state), pin); 		
 }
 
 inline void read_pin(unsigned char pin)
